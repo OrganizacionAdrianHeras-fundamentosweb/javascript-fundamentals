c87cbff6bfc1e01c5e3d672e9f1436f1d3a82f59
rolandoandre123 <rbardalespriale@gmail.com>
2023-05-09 03:18:36 -0500

    add : sintaxis y tipo de dato primitivo javascript

---
+++ lec1-sintaxsis-tipodato/js/scripts.js
@@ -0,0 +1,21 @@
+// let numero = 5;
+// let numero = 4.5;
+let numero = -4;
+// let palabra = 'Hola Mundo';
+let palabra = "Hola Mundo";
+// let respuesta = true;
+let respuesta = false;
+
+const PI = 3.14;
+
+console.log(numero);
+console.log(palabra);
+console.log(respuesta);
+console.log(PI);
+
+numero = 54;
+palabra = 'ADIOS';
+
+console.log(numero);
+console.log(palabra);
+
